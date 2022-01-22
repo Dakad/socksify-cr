e81383af00c8ee139b42346d9ba3f48be2334855
DakaD <3106338+Dakad@users.noreply.github.com>
2022-01-22 17:58:23 +0100

    Add example script

--- spec/proxy_spec.cr
+++ spec/proxy_spec.cr
@@ -9,29 +9,22 @@ describe Socksify::HTTPClient do
     response = Socksify::HTTPClient.new(host) do |client|
       client.exec("GET", "/all")
     end
-    p! response
     response.success?.should eq(true)
   end
 
-  it "connect to a website and get a response using explicit proxy", focus: true do
+  it "connect to a website and get a response using explicit proxy", do
     host = URI.parse("https://ifconfig.me")
     client = Socksify::HTTPClient.new(host, ignore_env: true)
     # proxy = Socksify::Proxy.new("socks://104.131.8.62:10808")
-    # proxy = Socksify::Proxy.new("http://185.156.35.156:57367")
-    # proxy = Socksify::Proxy.new("socks5://198.199.109.36:62287")
-    # proxy = Socksify::Proxy.new("socks5://198.199.109.36:62287")
-    # proxy = Socksify::Proxy.new("socks4://181.236.221.138:4145")
-    proxy = Socksify::Proxy.new("http://140.227.238.217:3128")
+    # proxy = Socksify::Proxy.new("http://140.227.238.217:3128")
     # proxy = Socksify::Proxy.new("socks5://144.76.224.49:63640")
     # proxy = Socksify::Proxy.new("socks://202.107.74.24:7302")
+    proxy = Socksify::Proxy.new("http://45.72.3.134:8118")
     client.set_proxy(proxy)
     response = client.exec("GET", "/ip")
-    p! response.body
     response.success?.should eq(true)
     response.body.to_s.should eq(proxy.proxy_host)
-    response = client.exec("GET", "/all")
-    pp response.body
-client.close
+    client.close
   end
 
 end
