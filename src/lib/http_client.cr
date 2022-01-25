522329bac90caefc92bff410d7ea04a114929211
DakaD <3106338+Dakad@users.noreply.github.com>
2022-01-25 23:17:09 +0100

    Adapt HTTPClient#set_proxy and #io  methods to use Proxy.fallback_proxy if defined (WIP)

--- src/lib/http_client.cr
+++ src/lib/http_client.cr
@@ -8,9 +8,9 @@ class Socksify::HTTPClient < ::HTTP::Client
   getter proxy_url : String? = nil
 
   # Determine if HTTP request can skip proxy if failed to connect
-  @skip_proxy : Bool = false
+  getter skip_proxy : Bool = false
 
-  def set_proxy(proxy : Proxy = nil)
+  def set_proxy(proxy : Proxy = nil, is_fallback = false)
     socket = @io
     return if socket && !socket.closed?
 
@@ -21,8 +21,8 @@ class Socksify::HTTPClient < ::HTTP::Client
       end
     rescue e : IO::Error
       HTTPClient.logger.fatal "Proxy could not opened : #{e}"
-      abort 100 unless @skip_proxy
-      # @io = nil
+      #TODO: Instead of aborting, throw a specific Error
+      abort 1000 if is_fallback
     end
     HTTPClient.logger.debug "my HTTPClient#set_proxy  proxy.open->io  #{@io.inspect}"
   end
@@ -32,15 +32,23 @@ class Socksify::HTTPClient < ::HTTP::Client
     # HTTPClient.logger.debug "my HTTPClient->io #{io.inspect}"
     return io if io
 
-    set_proxy Proxy.new @proxy_url.not_nil! unless @proxy_url.nil?
-    super #if @skip_proxy
+    begin
+      set_proxy(Proxy.new @proxy_url.not_nil!) unless @proxy_url.nil?
+    rescue e : IO::Error  # TODO: Replace by the specific ConnectionError
+      if @skip_proxy && Proxy.has_fallback_proxy?
+        set_proxy(Proxy.new(Proxy.config.fallback_proxy_url), is_fallback: true)
+      end
+    end
+  rescue e : IO::Error  # TODO: Replace by the specific FallbackConnectionError
+    @skip_proxy ? super : raise e
+    #TODO Move the call super into ensure block so whatever we should try it
   end
 
-  def self.new(uri : URI, tls = nil, ignore_env = false)
+  def self.new(uri : URI, tls = nil, ignore_config = false)
     inst = super(uri, tls)
-    # if !ignore_env && Proxy.behind_proxy?
-    #   inst.set_proxy Proxy.new(*Socksify::Proxy.parse_proxy_url)
-    # end
+    if !ignore_config && Proxy.has_fallback_proxy?
+      inst.set_proxy Proxy.new(Proxy.config.fallback_proxy_url)
+    end
     inst
   end
 
