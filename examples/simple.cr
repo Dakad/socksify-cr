c82b4209cc5f8d037ea078f7b7e15dedd2ce35be
DakaD <3106338+Dakad@users.noreply.github.com>
2022-02-03 21:36:15 +0100

    Update HTTPClient to handle FallbackOpenConnection

--- examples/simple.cr
+++ examples/simple.cr
@@ -2,14 +2,14 @@ require "../src/socksify"
 
 Socksify::Proxy.configure do |config|
   config.timeout_sec = 50
-  config.max_retries = 2
-  # config.fallback_proxy_uri = "socks5://127.0.0.1:9150"
+  config.max_retries = 0
+  config.fallback_proxy_url = "socks5://127.0.0.1:9150"
 end
 
   host = "https://ipconfig.me"
-  client = Socksify::HTTPClient.new(URI.parse(host), ignore_config: true)
+  client = Socksify::HTTPClient.new(URI.parse(host), ignore_config: false)
 
-  proxy = Socksify::Proxy.new("socks://192.111.130.5:17002")
+  # proxy = Socksify::Proxy.new("socks://192.111.130.5:17002")
   # proxy = Socksify::Proxy.new("socks5://198.199.109.36:62287")
   # proxy = Socksify::Proxy.new("socks4://181.236.221.138:4145")
   # proxy = Socksify::Proxy.new("http://140.227.238.217:3128")
@@ -17,7 +17,7 @@ end
   # proxy = Socksify::Proxy.new("socks://127.0.0.1:9150")
 
   begin
-  client.set_proxy(proxy)
+  # client.set_proxy(proxy)
   client.skip_proxy = true
   response = client.exec("GET", "/ip", headers: HTTP::Headers{"User-Agent" => "curl/7.80.0"})
   pp response.body
--- src/lib/http_client.cr
+++ src/lib/http_client.cr
@@ -11,7 +11,7 @@ class Socksify::HTTPClient < ::HTTP::Client
   # if failed to connect during the exec_request
   setter skip_proxy : Bool = false
 
-  def set_proxy(proxy : Proxy = nil)
+  def set_proxy(proxy : Proxy = nil, is_fallback = true)
     socket = @io
     return if socket && !socket.closed?
 
@@ -22,7 +22,11 @@ class Socksify::HTTPClient < ::HTTP::Client
       @proxy_url = proxy.proxy_url.not_nil!
     rescue e : IO::Error | Socksify::SOCKSError
       error_msg = "Failed to open connection on proxy '#{proxy.proxy_url}'"
-      raise Socksify::ProxyError::OpenConnectionError.new(error_msg, e)
+      unless is_fallback
+        raise Socksify::ProxyError::OpenConnectionError.new(error_msg, e)
+      else
+        raise Socksify::ProxyError::FallbackOpenConnectionError.new(error_msg, e)
+      end
     end
     HTTPClient.logger.debug "my HTTPClient#set_proxy  proxy.open->io  #{@io.inspect}"
   end
@@ -31,22 +35,32 @@ class Socksify::HTTPClient < ::HTTP::Client
     io = @io
     return io if io && !io.closed?
 
-    set_proxy Proxy.new @proxy_url.not_nil!
+    begin
+      set_proxy Proxy.new @proxy_url.not_nil!
+    rescue e : ProxyError::OpenConnectionError
+      if Proxy.has_fallback_proxy?
+        set_proxy(Proxy.new(Proxy.config.fallback_proxy_url), is_fallback: true)
+      else
+        raise e
+      end
+    end
     return @io.not_nil!
-  rescue e : ProxyError::OpenConnectionError
-    @skip_proxy ? return super  : raise e
+  rescue e : ProxyError
+    @skip_proxy ? return super : raise e
   end
 
-  def self.new(uri : URI, tls = nil, ignore_config = false)
+  def self.new(uri : URI, tls = nil, ignore_config = false, use_fallback = false)
     inst = super(uri, tls)
-    # if !ignore_config && Proxy.behind_proxy?
-    #   inst.set_proxy Proxy.new(*Socksify::Proxy.parse_proxy_url)
-    # end
+    # Fallback proxy configured
+    if use_fallback || (!ignore_config && Proxy.config.fallback_proxy_url?)
+      inst.set_proxy(Proxy.new(Proxy.config.fallback_proxy_url), is_fallback: true)
+    end
+
     inst
   end
 
-  def self.new(uri : URI, tls = nil, ignore_config = false)
-    yield new(uri, tls, ignore_config)
+  def self.new(uri : URI, tls = nil, ignore_config = false, use_fallback = false)
+    yield new(uri, tls, ignore_config, use_fallback)
   end
 
   def proxy_connection_options
